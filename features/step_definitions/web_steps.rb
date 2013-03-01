1ebcee7f015504b44dea21ac7190adb69e72b510
Paul Scarrone <samuraipanzer@Pauls-MacBook-Pro.local>
2013-03-01 11:13:30 -0500

    Updated step defs

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -38,6 +38,13 @@ Then /^I should not see "(.*)"$/ do |text|
   page.body.to_s.should_not =~ /#{text}/m
 end
 
+#https://github.com/daneroo/capybara-phantomjs/blob/master/features/step_definitions/web_steps.rb
+When /^I wait (\d+) seconds?$/ do |seconds|
+  sleep seconds.to_i
+  stamp = Time.now.strftime("%Y%m%d%H%M%S");
+  save_screenshot("screen-#{stamp}.png")
+end
+
 When /^I sign in as "(.*?)"$/ do |arg1|
   fill_in("username", :with => arg1.split("/")[0])
   fill_in("password", :with => arg1.split("/")[1])
@@ -56,4 +63,10 @@ end
 
 When /^I click the "([^"]*)" button$/ do |button_text|
   click_button button_text
+end
+
+#https://github.com/mrako/standalone-cucumber/blob/master/features/step_definitions/web_steps.rb
+
+Then /^show me the page$/ do
+  save_and_open_page
 end
